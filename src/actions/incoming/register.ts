8a76587b489b7b79bef5f676b4799450df417bdc
lewla <git@lew.la>
2025-06-09 18:46:48 +0100

    Validate data on registration

--- src/actions/incoming/register.ts
+++ src/actions/incoming/register.ts
@@ -18,8 +18,8 @@ export class RegisterAction extends BaseAction {
     public static identifier = 'register'
     public body: { data: RegisterData }
 
-    constructor (sender: WebSocket, body: { data: RegisterData }) {
-        super(sender, body)
+    constructor (sender: WebSocket, body: { data: RegisterData }, id?: string) {
+        super(sender, body, id)
         this.body = body
 
         if (
@@ -32,6 +32,38 @@ export class RegisterAction extends BaseAction {
     }
 
     public async handle (): Promise<void> {
+        this.body.data.username = this.body.data.username.toLowerCase()
+
+        if (this.body.data.username.length < 2) {
+            sendError(this.sender, 'Username is too short', this.id)
+            return
+        }
+        if (this.body.data.username.length > 32) {
+            sendError(this.sender, 'Username is too long', this.id)
+            return
+        }
+        if (!/^[a-z0-9._]+$/.test(this.body.data.username)) {
+            sendError(this.sender, 'Username contains invalid characters', this.id)
+            return
+        }
+        if (!/[a-z0-9]/.test(this.body.data.username)) {
+            sendError(this.sender, 'Username must contain at least one alphanumeric character', this.id)
+            return
+        }
+        if (/[._]{2,}/.test(this.body.data.username)) {
+            sendError(this.sender, 'Username cannot contain consequtive dots or underscores', this.id)
+            return
+        }
+
+        if (this.body.data.password.length < 6) {
+            sendError(this.sender, 'Password is too short', this.id)
+            return
+        }
+        if (this.body.data.password.length > 128) {
+            sendError(this.sender, 'Password is too long', this.id)
+            return
+        }
+
         const passwordHash = await hash(this.body.data.password)
 
         const memberId = randomUUID()
